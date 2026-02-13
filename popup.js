cbb30350b4f784c186a37d187b1ab7b5d989b034
shasanudin <shidiqhasanudin6@gmail.com>
2026-02-13 23:31:25 +0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: 'getStatus'}, function(response) {
+            if (response) {
+                document.getElementById('balance').textContent = response.balance.toFixed(8);
+            }
+        });
+    });
+    
+    document.getElementById('withdraw').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: 'withdraw'});
+        });
+    });
+});
